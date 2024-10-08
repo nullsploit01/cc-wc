b55e07fe3264bc8f6e17946bc423e938d177cadd
nullsploit01 <harshaldharmik099@gmail.com>
2024-10-09 01:29:29 +0530

    add unit tests

--- cmd/utils/utils_test.go
+++ cmd/utils/utils_test.go
@@ -1,51 +1,67 @@
 package utils_test
 
-// import (
-// 	"testing"
+import (
+	"os"
+	"testing"
 
-// 	"github.com/nullsploit01/cc-wc/cmd/utils"
-// )
+	"github.com/nullsploit01/cc-wc/cmd/utils"
+)
 
-// func TestByteCount(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		input    string
-// 		expected int
-// 	}{
-// 		{"empty string", "", 0},
-// 		{"ascii string", "hello", 5},
-// 		{"unicode string", "こんにちは", 15}, // Each Japanese character is 3 bytes in UTF-8.
-// 	}
+func createMockFile(data string) *os.File {
+	r, w, _ := os.Pipe()
+	w.Write([]byte(data))
+	w.Close()
+	return r
+}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			result := utils.ByteCount(tt.input)
-// 			if result != tt.expected {
-// 				t.Errorf("ByteCount(%q) = %d, want %d", tt.input, result, tt.expected)
-// 			}
-// 		})
-// 	}
-// }
+func TestByteCount(t *testing.T) {
+	data := "Hello, World!\nThis is a test string.\n"
+	file := createMockFile(data)
+	defer file.Close()
 
-// func TestLineCount(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		input    string
-// 		expected int
-// 	}{
-// 		{"empty string", "", 1},
-// 		{"one line", "hello", 1},
-// 		{"two lines unix", "hello\nworld", 2},
-// 		{"two lines windows", "hello\r\nworld", 2},
-// 		{"multiple lines", "line1\nline2\nline3", 3},
-// 	}
+	expected := 39 // byte count of data
+	result := utils.ByteCount(file)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			result := utils.LineCount(tt.input)
-// 			if result != tt.expected {
-// 				t.Errorf("LineCount(%q) = %d, want %d", tt.input, result, tt.expected)
-// 			}
-// 		})
-// 	}
-// }
+	if result != expected {
+		t.Errorf("ByteCount was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestLineCount(t *testing.T) {
+	data := "First line\nSecond line\nThird line\n"
+	file := createMockFile(data)
+	defer file.Close()
+
+	expected := 3 // number of lines
+	result := utils.LineCount(file)
+
+	if result != expected {
+		t.Errorf("LineCount was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestWordCount(t *testing.T) {
+	data := "Hello, who are you?\nAre you testing this?"
+	file := createMockFile(data)
+	defer file.Close()
+
+	expected := 8 // number of words
+	result := utils.WordCount(file)
+
+	if result != expected {
+		t.Errorf("WordCount was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestCharacterCount(t *testing.T) {
+	data := "123456789\n"
+	file := createMockFile(data)
+	defer file.Close()
+
+	expected := 10 // 9 digits + 1 newline
+	result := utils.CharacterCount(file)
+
+	if result != expected {
+		t.Errorf("CharacterCount was incorrect, got: %d, want: %d.", result, expected)
+	}
+}
